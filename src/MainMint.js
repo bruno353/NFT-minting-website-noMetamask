ffcc0d8ab71707a008d9e714a978b84cdba7ec4d
Bruno Laureano <brunolsantos152@gmail.com>
2022-09-07 18:50:43 -0300

    update

--- src/MainMint.js
+++ src/MainMint.js
@@ -2,17 +2,76 @@ import { useState } from "react";
 import { ethers, BigNumber } from "ethers";
 import { Box, Button, Flex, Input, Text } from "@chakra-ui/react";
 import roboPunksNFT from "./RoboPunksNFT.json";
+import { ConsoleSqlOutlined, LoadingOutlined } from '@ant-design/icons'
+import { Spin, Row, Col } from "antd";
+var axios = require('axios');
 
-const roboPunksNFTAddress = "0xBA7d0E7d36b733ff8113980a263cde09E3129Be3";
 
 const MaintMint = ({ accounts, setAccounts }) => {
   const [mintAmount, setMintAmount] = useState(1);
   const isConnected = Boolean(accounts[0]);
+  const [isMinting, setIsMinting] = useState(false);
+  const [confirmTrans, setConfirmTrans] = useState(false);
+  const [userAddress, setUserAddress] = useState('');
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  async function chamadaAPI() {
+    //e.preventDefault();
+    setIsMinting(true)
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      console.log(provider)
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
+
+      var data = JSON.stringify({
+        "quantity": "0.01",
+        "metamask": address,
+        "name": title,
+        "email": body
+      });
+      
+      var config = {
+        method: 'post',
+        url: 'https://parseapi.back4app.com/functions/swapPix',
+        headers: { 
+          'X-Parse-Application-Id': 'cACXAALjoAERRdB7jMAPSvMwBAfv7MC2yebDYxSw', 
+          'X-Parse-REST-API-Key': 'iDcYvliNQkyCRm0L52ca2ghI85cyVNa9zAYI6Xus', 
+          'Content-Type': 'application/json'
+        },
+        data : data
+      };
+      
+      await axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data.result.charge.paymentLinkUrl));
+        window.location.replace(response.data.result.charge.paymentLinkUrl);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    }
+
+
+  }
+
+  function cancel() {
+    setConfirmTrans(false)
+  }
 
   async function handleMint() {
     if (window.ethereum) {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
+      console.log(provider)
       const signer = provider.getSigner();
+      const address = await signer.getAddress();
+      await setUserAddress(address);
+      if(body.includes("@") && title !== '' && body.includes(".")){
+      console.log(address)}
+      setConfirmTrans(true)
+            /*
       const contract = new ethers.Contract(
         roboPunksNFTAddress,
         roboPunksNFT.abi,
@@ -26,6 +85,13 @@ const MaintMint = ({ accounts, setAccounts }) => {
       } catch (err) {
         console.log("error: ", err);
       }
+      */
+     if(body.includes("@") == false || body.includes(".") == false){
+      alert("Por favor, ensira um e-mail válido")
+     }
+     if(body == '' || title == ''){
+      alert("Por favor, preencha todos os campos")
+     }
     }
   }
 
@@ -38,13 +104,58 @@ const MaintMint = ({ accounts, setAccounts }) => {
     if (mintAmount >= 3) return;
     setMintAmount(mintAmount + 1);
   };
+  const peido = 1
 
+  if (isMinting == true){
+    return(<div>
+      <h1>Carregando </h1>
+      <span><Spin indicator={<LoadingOutlined style={{ fontSize: 100 }} spin />} /></span>
+    </div>)}
+
+  if (confirmTrans == true) {
+    return (
+      <Flex justify="center" align="center" height="100vh" paddingBottom="150px" lineHeight="50px">
+        <Box width="1200px">
+          <h1>Você irá realizar a mintagem de {mintAmount} TBT NFT(s) para a carteira {userAddress}</h1>
+          <h2>Total de R$0,01</h2>
+          <Button
+              backgroundColor="red"
+              borderRadius="5px"
+              boxShadow="0px 2px 2px 1px #0F0F0F"
+              color="white"
+              cursor="pointer"
+              fontFamily="inherit"
+              padding="15px"
+              margin="10"
+              onClick={cancel}
+            >
+              Cancelar
+            </Button>
+          <Button
+              backgroundColor="#008fd4"
+              borderRadius="5px"
+              boxShadow="0px 2px 2px 1px #0F0F0F"
+              color="white"
+              cursor="pointer"
+              fontFamily="inherit"
+              padding="15px"
+              margin="10"
+              onClick={chamadaAPI}
+            >
+              Confirmar
+            </Button>
+        </Box>
+    </Flex>
+
+
+    )
+  }
   return (
     <Flex justify="center" align="center" height="100vh" paddingBottom="150px">
-      <Box width="520px">
+      <Box width="1200px">
         <div>
           <Text fontSize="48px" textShadow="0 5px #000000">
-            RoboPunksNFT
+            Trix Battle Tactics NFT
           </Text>
           <Text
             fontSize="30px"
@@ -52,12 +163,34 @@ const MaintMint = ({ accounts, setAccounts }) => {
             fontFamily="VT323"
             textShadow="0 2px 2px #000000"
           >
-            This website is for demo purposes only.
+            Minte o NFT com a sua metamask.
           </Text>
         </div>
 
         {isConnected ? (
           <div>
+            <form>
+                <input 
+                  type="text" 
+                  required
+                  placeholder="Nome" 
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  style={{ width: "300px", height:"35px", fontFamily: "inherit"}}
+                />
+                <div>
+                <div>
+                  <input 
+                  type="email" 
+                  required 
+                  placeholder="Email"
+                  value={body}
+                  onChange={(e) => setBody(e.target.value)}
+                  style={{ width: "300px", height:"35px", fontFamily: "inherit", marginTop: "15px"}}
+                />
+                </div>
+                </div>
+            </form>
             <Flex justify="center" align="center">
               <Button
                 backgroundColor="#008fd4"
@@ -77,7 +210,7 @@ const MaintMint = ({ accounts, setAccounts }) => {
               <Input
                 readOnly
                 fontFamily="inherit"
-                width="100px"
+                width="200px"
                 height="40px"
                 textAlign="center"
                 paddingLeft="19px"
@@ -113,7 +246,7 @@ const MaintMint = ({ accounts, setAccounts }) => {
               margin="10"
               onClick={handleMint}
             >
-              Mint Now
+              Mintar {mintAmount} NFT
             </Button>
           </div>
         ) : (
